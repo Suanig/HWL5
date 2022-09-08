0d8ad7e6c35398893088c7b0c83aa4c2bed392fb
Suanig <suanig88@gmail.com>
2022-09-08 16:17:48 +0300

    Create HWL5T1

---
+++ HWL5T1/Program.cs
@@ -0,0 +1,22 @@
+﻿int[] array = new int[8];
+Random rand = new Random();
+for (int i = 0; i < 8; i++)
+array[i] = rand.Next(100 , 999); 
+var str = string.Join(" ", array);
+
+
+int evenM(int[] array)
+{
+    int even = 0; 
+    for (int i = 0; i < 8; i++ )
+    {
+        if (array[i] % 2 == 0)
+        {
+            even++;
+        }
+    }
+    return even;
+}
+
+Console.WriteLine($"Сгенерированный массив из 8 чисел - {str}");
+Console.WriteLine($"Количество четных чисел в массиве - {evenM(array)}");
